e32a92bd0a1fcf7f2303f927676d2c22c0d4fcef
dlga7723@gmail.com <dlga7723@gmail.com>
2024-05-11 21:53:00 +0900

    edit navbar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -28,34 +28,20 @@ function HomePage() {
   };
 
   return (
-    <div className="home-container">
+    <div
+      className="home-container"
+      style={{ margin: "0", padding: "0", width: "100%", overflowX: "hidden" }}>
       <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
+        <Container style={{ justifyContent: "center" }}>
           <Navbar.Brand
-            href="#home"
-            style={{ color: "royalblue", fontWeight: "bold" }}>
+            href="/"
+            style={{
+              color: "royalblue",
+              fontWeight: "bold",
+              textAlign: "center",
+            }}>
             차수리
           </Navbar.Brand>
-          {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse> */}
         </Container>
       </Navbar>
 
